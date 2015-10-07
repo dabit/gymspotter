19c8578f14d38fc9f57c5011558bd2a6b36338bd
David Padilla <david@padilla.cc>
2015-10-06 23:42:10 -0500

    First timer version

---
+++ src/gymspotter.c
@@ -0,0 +1,72 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+static int s_timer = 0;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Up");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, "0");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  static char s_timer_buffer[9];
+  
+  int seconds = s_timer % 60;
+  int minutes = (s_timer % 3600) / 60;
+  int hours = s_timer / 3600;
+  
+  snprintf(s_timer_buffer, sizeof(s_timer_buffer), "%d:%d:%d", hours, minutes, seconds);
+  text_layer_set_text(text_layer, s_timer_buffer);
+  s_timer++;
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+	.load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+  
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
