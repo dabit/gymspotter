3227310d061a6342031688e20861b2d238d84077
David Padilla <david@padilla.cc>
2015-11-30 21:29:42 -0600

    Tweaks to the Chalk version

--- src/gymspotter.c
+++ src/gymspotter.c
@@ -2,10 +2,24 @@
 
 #if defined(PBL_RECT)
   #define DEVICE_WIDTH 144
-  #define DEVICE_HEIGHT 168   
+  #define DEVICE_HEIGHT 168
+  #define MAX_LAYER_POSITION 100
+  #define TIMER_FONT fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK)
+  #define TIMER_HEIGHT 38
+  #define REST_FONT fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
+  #define REST_HEIGHT 24
+  #define REST_BACKGROUND_COLOR GColorBlack
+  #define MAX_FONT fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
 #elif defined(PBL_ROUND)
   #define DEVICE_WIDTH 180
-  #define DEVICE_HEIGHT 180 
+  #define DEVICE_HEIGHT 180
+  #define MAX_LAYER_POSITION 120
+  #define TIMER_FONT fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD)
+  #define TIMER_HEIGHT 44
+  #define REST_FONT fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD)
+  #define REST_HEIGHT 32
+  #define REST_BACKGROUND_COLOR GColorRed
+  #define MAX_FONT fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)
 #endif
 
 static Window *window;
@@ -19,7 +33,6 @@ static bool s_timer_running = false;
 static const uint32_t const s_vibe_segments[] = { 1000, 300, 1000 };
 static const int const s_max_timer_settings[] = { 45, 60, 75, 90, 105, 120 };
 static GFont s_res_gothic_18_bold;
-static GFont s_res_bitham_30_black;
 
 static void set_rest_layer_visible(bool visibility) {
   layer_set_hidden(text_layer_get_layer(s_textlayer_rest), !visibility);
@@ -79,23 +92,21 @@ static void window_load(Window *window) {
   if(persist_exists(0)) {
     s_max_timer = persist_read_int(0);
   }
-  s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
-  s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
 
-  s_textlayer_timer = text_layer_create(GRect(0, (DEVICE_HEIGHT/2 - 19), DEVICE_WIDTH, 38));
+  s_textlayer_timer = text_layer_create(GRect(0, (DEVICE_HEIGHT/2 - TIMER_HEIGHT/2), DEVICE_WIDTH, TIMER_HEIGHT));
   text_layer_set_text(s_textlayer_timer, "00:00");
   text_layer_set_text_alignment(s_textlayer_timer, GTextAlignmentCenter);
-  text_layer_set_font(s_textlayer_timer, s_res_bitham_30_black);
+  text_layer_set_font(s_textlayer_timer, TIMER_FONT);
 
-  s_textlayer_rest = text_layer_create(GRect(0, 20, DEVICE_WIDTH, 24));
-  text_layer_set_background_color(s_textlayer_rest, GColorBlack);
+  s_textlayer_rest = text_layer_create(GRect(0, 20, DEVICE_WIDTH, REST_HEIGHT));
+  text_layer_set_background_color(s_textlayer_rest, REST_BACKGROUND_COLOR);
   text_layer_set_text_color(s_textlayer_rest, GColorWhite);
   text_layer_set_text(s_textlayer_rest, "REST");
   text_layer_set_text_alignment(s_textlayer_rest, GTextAlignmentCenter);
-  text_layer_set_font(s_textlayer_rest, s_res_gothic_18_bold);
+  text_layer_set_font(s_textlayer_rest, REST_FONT);
   set_rest_layer_visible(false);
   
-  s_textlayer_max = text_layer_create(GRect(0, 100, DEVICE_WIDTH, 48));
+  s_textlayer_max = text_layer_create(GRect(0, MAX_LAYER_POSITION, DEVICE_WIDTH, 48));
   text_layer_set_background_color(s_textlayer_max, GColorWhite);
   text_layer_set_text_color(s_textlayer_max, GColorBlack);
 
@@ -104,7 +115,7 @@ static void window_load(Window *window) {
 
   text_layer_set_text(s_textlayer_max, s_max_buffer);
   text_layer_set_text_alignment(s_textlayer_max, GTextAlignmentCenter);
-  text_layer_set_font(s_textlayer_max, s_res_gothic_18_bold);
+  text_layer_set_font(s_textlayer_max, MAX_FONT);
 
   layer_add_child(window_layer, text_layer_get_layer(s_textlayer_timer));
   layer_add_child(window_layer, text_layer_get_layer(s_textlayer_rest));
